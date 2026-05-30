8f8a305c6e2aa844849bf21df61f19b51292ed90
Joelsalomon <munyanezajoel11@gmail.com>
2026-05-30 15:58:25 +0200

    Delete Desktop/stock management/restaurant-stock-management directory

--- Desktop/stock management/restaurant-stock-management/restaurant-stock-management-3/restaurant_stock_management/wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_stock_management.settings')
-
-from django.core.wsgi import get_wsgi_application
-
-application = get_wsgi_application()
